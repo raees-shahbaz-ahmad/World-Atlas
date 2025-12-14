a70390bc027fb8edda69c86cb08749124c2939a4
raees-shahbaz-ahmad <shahbazahmad7472@gmail.com>
2025-12-14 11:15:28 +0500

    Fix UI folder case issue

--- src/components/Layout/AppLayout.jsx
+++ src/components/Layout/AppLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Headers from '../UI/Headers'
-import Footers from '../UI/Footers'
+import Headers from '../UI/Headers.jsx'
+import Footers from '../UI/Footers.jsx'
 import { Outlet } from 'react-router-dom'
 
 const AppLayout = () => {
